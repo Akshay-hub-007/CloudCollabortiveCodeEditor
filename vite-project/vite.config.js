6dd088216c06f907a08518d5c3284a4589cd3c52
Akshay Kalangi <akshaykalangi9@gmail.com>
2024-12-07 22:43:01 +0530

    update code

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 4173, // Use the PORT environment variable
+    host: true, // Allow network access
+  },
   plugins: [react()],
 })
